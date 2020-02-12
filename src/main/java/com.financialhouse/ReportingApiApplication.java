b0a69df04cd3afa6af5ef0180c13cb10e7ae8bc9
Sinan <snncskn@users.noreply.github.com>
2020-02-12 22:22:58 +0300

    initial commit

---
+++ src/main/java/com.financialhouse/ReportingApiApplication.java
@@ -0,0 +1,15 @@
+package com.financialhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author Sinan
+ */
+@SpringBootApplication
+public class ReportingApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ReportingApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/financialhouse/ReportingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
