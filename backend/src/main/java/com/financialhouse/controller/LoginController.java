8b5f88d31927244cd710553acf0aa3adc5c46859
Sinan COSKUN <Cskn6396993>
2020-02-20 11:48:19 +0300

    code clean & checkstyle

--- backend/src/main/java/com/financialhouse/controller/LoginController.java
+++ backend/src/main/java/com/financialhouse/controller/LoginController.java
@@ -5,7 +5,6 @@ import com.financialhouse.dto.form.request.LoginCredentialsForm;
 import com.financialhouse.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +17,6 @@ import javax.validation.Valid;
  */
 
 @Slf4j
-@CrossOrigin
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
